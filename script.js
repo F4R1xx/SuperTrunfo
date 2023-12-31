7870d7ff3d78ac43e069be2e679ae36e844d6c63
F4R1x <ibmecfagundes@gmail.com>
2023-12-31 16:08:34 -0300

    Versão 1.0

---
+++ script.js
@@ -0,0 +1,200 @@
+const cardsFluminense = [
+  //Normal
+    { name: 'German Cano', Defesa: 59, Control: 64, Ataque: 81, Over: 204, image: 'Fluminense/GermanCano.png' },
+    { name: 'Keno', Defesa: 58, Control: 68, Ataque: 72, Over: 198, image: 'Fluminense/Keno.png' },
+    { name: 'Jhon Arias', Defesa: 62, Control: 65, Ataque: 73, Over: 200, image: 'Fluminense/JhonArias.png' },
+    { name: 'Matheus Martinelli', Defesa: 61, Control: 69, Ataque: 60, Over: 190, image: 'Fluminense/MatheusMartinelli.png' },
+    { name: 'P. H. Ganso', Defesa: 55, Control: 75, Ataque: 67, Over: 197, image: 'Fluminense/Ganso.png' },
+    { name: 'Andre', Defesa: 76, Control: 74, Ataque: 65, Over: 215, image: 'Fluminense/Andre.png' },
+    { name: 'Marcelo', Defesa: 78, Control: 70, Ataque: 63, Over: 211, image: 'Fluminense/Marcelo12.png' },
+    { name: 'Nino', Defesa: 83, Control: 65, Ataque: 49, Over: 197, image: 'Fluminense/Nino.png' },
+    { name: 'Felipe Melo', Defesa: 78, Control: 70, Ataque: 43, Over: 191, image: 'Fluminense/FelipeMelo.png' },
+    { name: 'Samuel Xavier', Defesa: 75, Control: 65, Ataque: 50, Over: 190, image: 'Fluminense/SamuelXavier.png' },
+    { name: 'Fabio', Defesa: 79, Control: 56, Ataque: 40, Over: 175, image: 'Fluminense/Fabio.png' },
+    //Libertadores
+    { name: 'Nino Libertadores', Defesa: 93, Control: 72, Ataque: 63, Over: 228, image: 'Fluminense/NinoLibertadores.png' },
+    { name: 'Felipe Melo Libertadores', Defesa: 89, Control: 75, Ataque: 65, Over: 229, image: 'Fluminense/FelipeMeloLibertadores.png' },
+    { name: 'Samuel Xavier Libertadores', Defesa: 87, Control: 59, Ataque: 79, Over: 225, image: 'Fluminense/SamuelXavierLibertadores.png' },
+    { name: 'John Kennedy Libertadores', Defesa: 66, Control: 71, Ataque: 90, Over: 227, image: 'Fluminense/JohnKennedyLibertadores.png' },
+    { name: 'German Cano Libertadores', Defesa: 69, Control: 73, Ataque: 92, Over: 234, image: 'Fluminense/GermanCanoLibertadores.png' },
+    //Idolos
+    { name: 'Fred Idolo', Defesa: 65, Control: 71, Ataque: 86, Over: 222, image: 'Fluminense/FredIdolo.png' },
+    { name: 'Conca Idolo', Defesa: 65, Control: 86, Ataque: 70, Over: 221, image: 'Fluminense/Conca.png' },
+    { name: 'Washington Idolo', Defesa: 65, Control: 68, Ataque: 81, Over: 214, image: 'Fluminense/Washington.png' },
+    //Craques
+    { name: 'John Kennedy Craque', Defesa: 65, Control: 86, Ataque: 70, Over: 221, image: 'Fluminense/JohnKennedyCraque.png' },
+    // Adicione mais cartas conforme necessário
+  ];
+
+const cardsFlamengo = [
+  //Normais
+    { name: 'Pedro', Defesa: 55, Control: 65, Ataque: 78, Over: 198, image: 'Flamengo/Pedro.png' },
+    { name: 'Everton Cebolinha', Defesa: 61, Control: 70, Ataque: 75, Over: 206, image: 'Flamengo/EvertonCebolinha.png' },
+    { name: 'Bruno Henrique', Defesa: 65, Control: 72, Ataque: 77, Over: 214, image: 'Flamengo/BrunoHenrique.png' },
+    { name: 'Arrascaeta', Defesa: 62, Control: 78, Ataque: 68, Over: 208, image: 'Flamengo/Arrascaeta.png' },
+    { name: 'Gerson', Defesa: 60, Control: 72, Ataque: 67, Over: 199, image: 'Flamengo/Gerson.png' },
+    { name: 'Erick Pulgar', Defesa: 62, Control: 70, Ataque: 64, Over: 196, image: 'Flamengo/ErickPulgar.png' },
+    { name: 'Ayrton Lucas', Defesa: 76, Control: 68, Ataque: 65, Over: 209, image: 'Flamengo/AyrtonLucas.png' },
+    { name: 'Léo Pereira', Defesa: 77, Control: 59, Ataque: 58, Over: 194, image: 'Flamengo/LeoPereira.png' },
+    { name: 'Fabrício Bruno', Defesa: 72, Control: 65, Ataque: 55, Over: 192, image: 'Flamengo/FabricioBruno.png' },
+    { name: 'Matheuzinho', Defesa: 72, Control: 68, Ataque: 63, Over: 203, image: 'Flamengo/Matheuzinho.png' },
+    { name: 'Rossi ', Defesa: 74, Control: 55, Ataque: 40, Over: 169, image: 'Flamengo/AgustinRossi.png' },
+    //Craques
+    { name: 'Gabriel Barbosa Craque', Defesa: 69, Control: 72, Ataque: 83, Over: 224, image: 'Flamengo/GabrielBarbosa.png' },
+    { name: 'Everton Ribeiro Craque', Defesa: 68, Control: 80, Ataque: 73, Over: 221, image: 'Flamengo/EvertonRibeiro.png' },
+    { name: 'Felipe Luís Craque', Defesa: 82, Control: 75, Ataque: 69, Over: 226, image: 'Flamengo/FelipeLuis.png' },
+    //Idolos
+    { name: 'Zico Idolo', Defesa: 65, Control: 94, Ataque: 93, Over: 252, image: 'Flamengo/Zico.png' },
+    { name: 'Petkovic Idolo', Defesa: 63, Control: 86, Ataque: 76, Over: 225, image: 'Flamengo/Petkovic.png' },
+    { name: 'Adriano Idolo', Defesa: 67, Control: 71, Ataque: 90, Over: 228, image: 'Flamengo/AdrianoImperador.png' },
+    // Adicione mais cartas conforme necessário
+  ];
+
+  let playerHand = [];
+  let computerHand = [];
+  let selectedCardIndex = null;
+  let selectedAttribute = 'Defesa';
+  let playerScore = 0;
+  let computerScore = 0;
+
+  function startGame() {
+    // Embaralhar as cartas
+    cardsFluminense.sort(() => Math.random() - 0.5);
+    // Distribuir apenas 4 cartas entre o jogador e o computador
+    playerHand = cardsFluminense.slice(0, 7);
+
+    cardsFlamengo.sort(() => Math.random() - 0.5);
+    computerHand = cardsFlamengo.slice(0, 7);
+
+    updateHandUI();
+    updateScoreUI();
+  }
+
+  function updateHandUI() {
+    const playerHandElement = document.getElementById('player-hand');
+    const computerHandElement = document.getElementById('computer-hand');
+
+    playerHandElement.innerHTML = '<h2>Seu Deck</h2>';
+    computerHandElement.innerHTML = '<h2>Deck do Computador</h2>';
+
+    playerHand.forEach((card, index) => {
+      const cardElement = createCardElement(card, index);
+      playerHandElement.appendChild(cardElement);
+    });
+
+    computerHand.forEach((card, index) => {
+      const cardElement = createCardElement(card, index, true);
+      computerHandElement.appendChild(cardElement);
+    });
+  }
+
+  function createCardElement(card, index, isComputer = false) {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+    if (isComputer) {
+      cardElement.classList.add('computer-card');
+    } else {
+      cardElement.dataset.index = index;
+      cardElement.onclick = () => selectCard(index);
+    }
+
+    // Adiciona a imagem à carta
+    const imageElement = document.createElement('img');
+    imageElement.src = card.image;
+    cardElement.appendChild(imageElement);
+    /*
+    const attributesHTML = Object.entries(card).map(([key, value]) => `<strong>${key}:</strong> ${value}`).join('<br>');
+    cardElement.innerHTML += `<div>${attributesHTML}</div>`;
+    */
+    return cardElement;
+  }
+
+  function selectCard(index) {
+    // Adiciona a seleção ao card atual
+    const selectedCard = document.querySelector(`#player-hand .card[data-index="${index}"]`);
+
+    if (selectedCard.classList.contains('selected')) {
+      // Se já estiver selecionado, desmarque a seleção
+      selectedCard.classList.remove('selected');
+      selectedCardIndex = null;
+      document.getElementById('playButton').disabled = true;
+    } else {
+      // Remova a seleção do card anterior (se houver)
+      const previousSelectedCard = document.querySelector(`#player-hand .card.selected`);
+      if (previousSelectedCard) {
+        previousSelectedCard.classList.remove('selected');
+      }
+
+      // Adiciona a seleção ao card atual
+      selectedCard.classList.add('selected');
+      selectedCardIndex = index;
+      document.getElementById('playButton').disabled = false;
+    }
+
+    document.getElementById('result').textContent = '';
+  }
+
+  function updateSelectedAttribute(value) {
+    selectedAttribute = value;
+  }
+
+  function play() {
+    if (selectedCardIndex !== null) {
+      const playerCard = playerHand.splice(selectedCardIndex, 1)[0];
+      const computerCard = computerHand.pop();
+
+      updateHandUI();
+
+      const playerAttributeValue = playerCard[selectedAttribute];
+      const computerAttributeValue = computerCard[selectedAttribute];
+
+      const resultElement = document.getElementById('result');
+      const scoreElement = document.getElementById('score');
+
+      let roundResult = '';
+      if (playerAttributeValue > computerAttributeValue) {
+        roundResult = 'Você ganhou a rodada!';
+        playerScore++;
+      } else if (playerAttributeValue < computerAttributeValue) {
+        roundResult = 'Você perdeu a rodada!';
+        computerScore++;
+      } else {
+        roundResult = 'Empate!';
+      }
+
+      resultElement.textContent = roundResult;
+      resultElement.style.opacity = 1; // Exibe o resultado com uma animação
+
+      setTimeout(() => {
+        resultElement.style.opacity = 0; // Oculta o resultado após alguns segundos
+      }, 2000);
+
+      document.getElementById('playButton').disabled = true;
+
+      updateScoreUI();
+
+      // Desabilitar o botão de jogar e exibir o botão de nova rodada (F5)
+      document.getElementById('playButton').disabled = true;
+      document.getElementById('new-round').style.display = 'block';
+
+      // Verificar se o jogo acabou
+      if (playerHand.length === 0 || computerHand.length === 0) {
+        endGame();
+      }
+    }
+  }
+
+  function updateScoreUI() {
+    document.getElementById('playerScore').textContent = playerScore;
+    document.getElementById('computerScore').textContent = computerScore;
+  }
+
+  function endGame() {
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = 'Fim de Jogo!';
+    document.getElementById('playButton').disabled = false;
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    startGame();
+  });
